f92d448b5bfd669f6c8881b384f3d011c1f4bf4d
XID <86798664+Alexid-ID@users.noreply.github.com>
2023-03-07 23:50:43 +0700

    update fontawesome folder

---
+++ src/views/components/sidebar.php
@@ -0,0 +1,35 @@
+<style>
+    @import "<?php echo url('src/public/css/style.css') ?>";
+
+    .sidebar {
+        position: fixed;
+        width: 150px;
+        overflow-y: auto;
+        scrollbar-width: none;
+
+
+        top: 0;
+        bottom: 0;
+        z-index: 10;
+        background-color: #005cbf;
+    }
+
+    ::-webkit-scrollbar {
+        display: none;
+    }
+</style>
+
+
+<div class="sidebar">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci corporis deserunt distinctio dolorem ea unde. Animi atque deleniti doloribus in, ipsum mollitia natus, nostrum odit optio, praesentium quos temporibus tenetur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci corporis deserunt distinctio dolorem ea unde. Animi atque deleniti doloribus in, ipsum mollitia natus, nostrum odit optio, praesentium quos temporibus tenetur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci corporis deserunt distinctio dolorem ea unde. Animi atque deleniti doloribus in, ipsum mollitia natus, nostrum odit optio, praesentium quos temporibus tenetur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci corporis deserunt distinctio dolorem ea unde. Animi atque deleniti doloribus in, ipsum mollitia natus, nostrum odit optio, praesentium quos temporibus tenetur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci corporis deserunt distinctio dolorem ea unde. Animi atque deleniti doloribus in, ipsum mollitia natus, nostrum odit optio, praesentium quos temporibus tenetur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci corporis deserunt distinctio dolorem ea unde. Animi atque deleniti doloribus in, ipsum mollitia natus, nostrum odit optio, praesentium quos temporibus tenetur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci corporis deserunt distinctio dolorem ea unde. Animi atque deleniti doloribus in, ipsum mollitia natus, nostrum odit optio, praesentium quos temporibus tenetur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci corporis deserunt distinctio dolorem ea unde. Animi atque deleniti doloribus in, ipsum mollitia natus, nostrum odit optio, praesentium quos temporibus tenetur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci corporis deserunt distinctio dolorem ea unde. Animi atque deleniti doloribus in, ipsum mollitia natus, nostrum odit optio, praesentium quos temporibus tenetur!
+    </p>
+</div>
