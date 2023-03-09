5f46bcc0bf477ee02f79e542fc5d9748851dcea3
S52100872 <52100872@student.tdtu.edu.vn>
2023-03-09 13:50:57 +0700

    updata database version 1

--- database.sql
+++ database.sql
@@ -6,69 +6,135 @@
 -- Generation Time: March 8 2023 at 23:59 P.M
 -- Server version: 8.0.31
 -- PHP Version: 8.1.6
-create database MusicData
-use MusicData
-create table `Advertises`(
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+-- CREATE DATABASE
+CREATE DATABASE MusicData;
+USE MusicData;
+-- 
+-- CREATE TABLE
+-- 
+-- CREATE TABLE Advertises
+-- 
+CREATE TABLE `Advertises`(
     `id_advertises` int PRIMARY KEY,
-    `title` VARCHAR(30),
-    `intro_image` VARCHAR(30),
-    `background` VARCHAR(30)
-)
-create table `Topics`(
+    `title` TEXT,
+    `intro_image` TEXT,
+    `background` TEXT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 
+-- CREATE TABLE Topics
+-- 
+CREATE TABLE `Topics`(
     `id_topics` int PRIMARY KEY,
-    `name_topics` VARCHAR(30),
-    `topic_image` VARCHAR(30)
-)
-create table `Categories`(
+    `name_topics` TEXT,
+    `topic_image` TEXT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 
+-- CREATE TABLE Categories
+-- 
+CREATE TABLE `Categories`(
     `id_cate` int PRIMARY KEY,
-    `name_cate` VARCHAR(30),
-    `cate_image` VARCHAR(30),
-    `id_topics` int,
-    -- FOREIGN KEY (id_topics) REFERENCES Topics(id_topics)
-)
-create table `Playlists`(
+    `name_cate` TEXT,
+    `cate_image` TEXT,
+    `id_topics` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 
+-- CREATE TABLE Playlists
+-- 
+CREATE TABLE `Playlists`(
     `id_playlist` int PRIMARY KEY,
-    `name_playlist` VARCHAR(30),
-    `playlist_image` VARCHAR(30),
+    `name_playlist` TEXT,
+    `playlist_image` TEXT,
     `create_at` DATE
-)
-create table `Artists`(
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 
+-- CREATE TABLE Artists
+-- 
+CREATE TABLE `Artists`(
     `id_artists` int PRIMARY KEY,
-    `bio` VARCHAR(30),
-    `name_artists` VARCHAR(30)
-)
-create table `Albums`(
+    `name_artists` TEXT,
+    `picture` TEXT,
+    `facebook` TEXT,
+    `instagram` TEXT,
+    `youtube` TEXT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 
+-- CREATE TABLE Albums
+-- 
+CREATE TABLE `Albums`(
     `id_alb` int PRIMARY KEY,
-    `name_alb` VARCHAR(30),
-    `id_artists` int,
-    -- FOREIGN KEY (id_artists) REFERENCES Artists(id_artists)
-)
-create table `Songs`(
+    `name_alb` TEXT,
+    `id_artists` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 
+-- CREATE TABLE Songs
+-- 
+CREATE TABLE `Songs`(
     `id_songs` int PRIMARY KEY,
-    `name_songs` VARCHAR(30),
+    `name_songs` TEXT,
     `duration` TIME,
     `release_date` DATE,
-    `id_cate`int,
+    `id_cate` int,
     `id_artists` int,
-    `id_advertises` int,
-    -- FOREIGN KEY (id_cate) REFERENCES Categories(id_cate),
-    -- FOREIGN KEY (id_artists) REFERENCES Artists(id_artists),
-    -- FOREIGN KEY (id_advertises) REFERENCES Advertises(id_advertises)
-)
-create table `Users`(
+    `id_advertises` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 
+-- CREATE TABLE Users
+-- 
+CREATE TABLE `Users`(
     `id_users` int PRIMARY KEY,
-    `username` VARCHAR(30),
-    `email` VARCHAR(30),
-    `pass` VARCHAR(30),
-    `phone` VARCHAR(10),
+    `username` TEXT,
+    `email_users` TEXT,
+    `pass_users` TEXT,
+    `phone_users` TEXT,
     `create_at` DATE,
-    `id_songs` int DEFAULT NULL,
-    -- FOREIGN KEY (id_songs) REFERENCES Songs(id_songs)
-)
-create table `Playlists_Songs`(
+    `role` INT,
+    `token_users` TEXT,
+    `id_songs` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 
+-- CREATE TABLE Playlists_Songs
+-- 
+CREATE TABLE `Playlists_Songs`(
     `id_playlist_song` int PRIMARY KEY,
     `id_songs` int,
-    `id_playlist` int,
-    -- FOREIGN KEY (id_songs) REFERENCES Songs(id_songs),
-    -- FOREIGN KEY (id_playlist) REFERENCES Playlists(id_playlist)
-)
+    `id_playlist` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 
+-- CREATE TABLE Admins
+-- 
+CREATE TABLE `Admins`(
+    `id_admins` int PRIMARY KEY,
+    `email_admins` TEXT,
+    `pass_admins` TEXT,
+    `token_admins` TEXT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 
+-- ADD FOREIGN KEY
+-- 
+-- FK for table Categories
+-- 
+ALTER TABLE `Categories`
+ADD CONSTRAINT `FK_id_topics_Topics_Categories` FOREIGN KEY (`id_topics`) REFERENCES Topics(`id_topics`);
+-- 
+-- FK for table Albums
+-- 
+ALTER TABLE `Albums`
+ADD CONSTRAINT `FK_id_artist_Albums_Artists` FOREIGN KEY (`id_artists`) REFERENCES Artists(`id_artists`);
+-- 
+-- FK for table Songs
+-- 
+ALTER TABLE `Songs`
+ADD CONSTRAINT `FK_id_cate_Songs_Categories` FOREIGN KEY (`id_cate`) REFERENCES Categories(`id_cate`);
+ALTER TABLE `Songs`
+ADD CONSTRAINT `FK_id_artist_Songs_Artists` FOREIGN KEY (`id_artists`) REFERENCES Artists(`id_artists`);
+ALTER TABLE `Songs`
+ADD CONSTRAINT `FK_id_adver_Songs_Advertises` FOREIGN KEY (`id_advertises`) REFERENCES Advertises(`id_advertises`);
+-- 
+-- FK for table Playlists_Songs
+-- 
+ALTER TABLE `Playlists_Songs`
+ADD CONSTRAINT `FK_id_songs_Playlists_Songs` FOREIGN KEY (`id_songs`) REFERENCES Songs(`id_songs`);
+ALTER TABLE `Playlists_Songs`
+ADD CONSTRAINT `FK_id_playlists_Playlists_Songs` FOREIGN KEY (`id_playlist`) REFERENCES Playlists(`id_playlist`);
