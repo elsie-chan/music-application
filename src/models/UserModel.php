388fd03a23a167fdd0bd305d3c24b3eeef4e6c88
2zz <kikinazzk98@gmail.com>
2023-03-09 22:09:18 +0700

    update Admin and User in Model
    add CRUD methods

--- src/models/UserModel.php
+++ src/models/UserModel.php
@@ -3,7 +3,7 @@
 use App\Model\Model;
 
 class UserModel extends Model {
-    protected $table = 'Users';
+    protected $table = 'users';
     public function __construct() {
         parent::__construct();
     }
@@ -25,18 +25,20 @@ class UserModel extends Model {
         $id = $this->countID()+1;
         $name = $_POST['username'];
         $email = $_POST["email"];
-        $sql = "SELECT * FROM `" .$this->table. "` WHERE `email` = '$email'";
-        $res = mysqli_query($this->con, $sql);
-        if(mysqli_num_rows($res) == 0 and $this->checkRegex($email,'/\w+@+[a-z]+\.+[a-z]+/gm')){
+        $sql = "SELECT * FROM $this->table WHERE `email_users` = '$email'";
+        $res = mysqli_num_rows(mysqli_query($this->con, $sql));
+        if($res == 0 and $this->checkRegex($email,'/\w+@+[a-z]+\.+[a-z]+/gm')){
             $email = $email;
+        }
+        else{
+            return;
         }
         $password = password_hash($_SESSION["password"], PASSWORD_DEFAULT);
         $mobile = $_POST["mobile-number"];
-        $mobile = $this->checkRegex($mobile,'^0[1-9][0-9]{8,9}$') ? $mobile : NULL;
+        $token = $_POST["token"];
         $date = date('Y-m-d H:i:s', time());
-        $conn = $this->con;
-        $stmt = $conn->prepare("INSERT INTO `.$this->table.` VALUES (?,?,?,?,?,?,?)");
-        $stmt-> bind_param("issssssi",$id,$name,$email,$password,$mobile,$date,NULL);
+        $sql = "INSERT INTO $this->table VALUES ('$id','$name','$email','$password','$mobile','$date','$token',0,NULL)";
+        $stmt = mysqli_query($this->con,$sql);
     }
     // Login User
     public function login() {
@@ -47,7 +49,7 @@ class UserModel extends Model {
         $username = $_POST["username"];
         $password = $_POST["password"];
 
-        $sql = "SELECT * FROM `" .$this->table. "` WHERE `email` LIKE '$email' or `username` LIKE '$username'";
+        $sql = "SELECT * FROM `" .$this->table. "` WHERE `email_users` LIKE '$email' or `username` LIKE '$username'";
         $res = mysqli_query($this->con, $sql);
 
         if (mysqli_num_rows($res) > 0) {
@@ -66,7 +68,48 @@ class UserModel extends Model {
         }
         return $response;
     }
-    // public function addSongs($id_songs){
-    //     $find_songs = "SELECT "
-    // }
+    // Find One User By ID
+    public function get_user_by_id($id_user){
+        $sql = "SELECT * FROM $this->table WHERE `id_users` = '$id_user'";
+        $stmt = mysqli_query($this->con, $sql);
+        if (mysqli_num_rows($stmt) > 0)
+            return mysqli_fetch_object($stmt);
+        else
+            return null;
+    }
+    // Find All Users
+    public function get_all_user(){
+        $sql = "SELECT * FROM `" . $this->table . "` ORDER BY `id_users` ASC";
+        $stmt = mysqli_query($this->con, $sql);
+        $data = array();
+        while($row = mysqli_num_rows($stmt)){
+            array_push($data,$row);
+        }
+        return $data;
+    }
+    // Update Profile User
+    public function update_profile($arr){
+        $temp = array($arr);
+        $sql = "UPDATE $this->table SET";
+        foreach($temp as $key => $val){
+            $sql = $sql.' '."$key = $val, ";
+        }
+        $sql = substr(trim($sql),0,-1);
+        return mysqli_query($this->con,$sql);
+    }
+    // Update Profile User
+    public function update_profile_where($arr1,$arr2){
+        $temp1 = array($arr1);
+        $temp2 = array($arr2);
+        $sql = "UPDATE $this->table SET";
+        foreach($temp1 as $key => $val){
+            $sql = $sql.' '."$key = $val, ";
+        }
+        $sql = substr(trim($sql),0,-1).' '."WHERE";
+        foreach($temp2 as $key => $val){
+            $sql = $sql.' '."$key = $val, ";
+        }
+        $sql = substr(trim($sql),0,-1);
+        return mysqli_query($this->con,$sql);
+    }
 }
