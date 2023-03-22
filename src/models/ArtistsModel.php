75f726a7f6f01a58c7632f42903b39401121b8e8
laquocbao2911 <laquocbao2911@gmail.com>
2023-03-22 17:22:21 +0700

    update db,admin,usermodel

---
+++ src/models/ArtistsModel.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Model\Model;
+
+class ArtistsModel extends Model
+{
+    protected $table = 'artists';
+    public function __construct()
+    {
+        parent::__construct();
+    }
+//    Create
+    function get_artist_by_username($username){
+
+    }
+//    Find
+//    Update
+//    Delete
+}
