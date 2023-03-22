b2e8485b04363bd644f7c7799b4a9e3ca02f35e3
Lam Nhu Trinh <lamnhutrinh@gmail.com>
2023-03-22 11:04:30 +0700

    add sign up

--- src/views/auth/auth.register.php
+++ src/views/auth/auth.register.php
@@ -1,59 +1,52 @@
-<!-- <form action="<?php echo url('auth/register')?>" method="POST">
-    <?php
-    $token = Security::create_token();
-    $_SESSION['token'] = $token;
-    ?>
-    <input type="hidden" name="token" id="token" value='<?php $token;?>'>
-    <input type="text" id="fname" name="username" placeholder="Username">
-    <input type="email" id="email" name="email" placeholder="Email">
-    <input type="password" id="password" name="password" placeholder="Password">
-    <input type="password" id="confirm_pass" name="confirm_pass" placeholder="Confirm password">
-    <input type="text" id="mobile" name="mobile" placeholder="Phone">
-    <input type="submit" id="submit" name="submit" value="Login">
-</form> -->
 <link rel="stylesheet" href="<?php echo url('src/public/vendors/bootstrap/css/bootstrap.css') ?>">
 <link rel="stylesheet" href="<?php echo url('src/public/vendors/font-awesome-6-pro-main/css/all.css') ?>">
+<link rel="stylesheet" href="<?php echo url('src/public/css/style.css') ?>">
 <style>
-    <?php require_once (assets('public/css/auth/register.css'))?>
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@300;400;500;700&display=swap');
+    <?php require_once (assets('public/css/auth/register.css')) ?>
     .bg {
-        background-image: url(<?php echo url('src/public/assets/imgs/bg_login.png') ?>);
+        background-image: url(<?php echo url('src/public/assets/imgs/bg_signup.png') ?>);
         background-size: cover;
     }
-    
 </style>
 <main>
     <div class="bg">
         <div class="container p-0">
             <div class="row d-flex justify-content-center">
-                <!-- <img src="<?php echo url('src/public/assets/imgs/bg_login1.png') ?>" class="" alt="Sample image"> -->
-                <div class="col-md-4 col-sm-6 col-6 modal-form glass">
-                        <form class="form-horizoltal form-login" action="<?php echo url('auth/register')?>" method="POST">
-                            <div class="form-logo">
-                                <img src="<?php echo url('src/public/assets/imgs/logo.svg') ?>" alt="">
-                            </div>
-                            <h3 class="title">Sign in to <span>Misc</span></h3>
-                            <div class="form-group d-flex justify-content-center ">
-                                <input class="input1 form-control glass" type="text" name="username" placeholder="Email address or username">
-                            </div>
-                            <div class="form-group d-flex justify-content-center ">
-                                <input class="input1 form-control glass" type="email" name="email" placeholder="Email address or username">
-                            </div>
-                            <div class="form-group d-flex justify-content-center">
-                                <input class="input2 form-control glass" type="password" name="password" placeholder="Password">
-                                <i class="fa-thin fa-arrow-right input-icon btn-submit"></i>
-                            </div>
-                            <div class="remember mt-3">
-                                <label><input type="checkbox"> Keep me signed in</label>
-                            </div>
-                            <hr class="hr hr-blurry" />
-                            <ul class="form-options">
-                                <li><a href="#">Forgot your password?</a></li>
-                                <li><a href="#"><span>Sign up for Misc</span></a></li>
-                            </ul>
-                        </form>
+                <div class="col-md-6 col-sm-8 col-8 modal-form glass">
+                    <form class="form-horizoltal form-login" action="<?php echo url('auth/login')?>" method="POST">
+                        <div class="form-logo">
+                            <img src="<?php echo url('src/public/assets/imgs/logo.svg') ?>" alt="">
+                        </div>
+                        <h3 class="title">Sign up for free to start listening <span>MISC</span></h3>
+                        <div class="separator d-flex justify-content-center">
+                            <hr class="line">
+                            <span>OR</span>
+                            <hr class="line">
+                        </div>
+                        <div class="form-group d-flex justify-content-center ">
+                            <input style="border-radius: 8px 8px 0 0; border-bottom: 0;" class="form-control glass" type="text" name="username" placeholder="Username">
+                        </div>
+                        <div class="form-group d-flex justify-content-center ">
+                            <input style="border-radius: 0; border-bottom: 0;" class="form-control glass" type="email" name="email" placeholder="Email address">
+                        </div>
+                        <div class="form-group d-flex justify-content-center">
+                            <input style="border-radius: 0; border-bottom: 0;" class="form-control glass" type="password" name="password" placeholder="Password">
+                        </div>
+                        <div class="form-group d-flex justify-content-center">
+                            <input style="border-radius: 0; border-bottom: 0;" class="form-control glass" type="password" name="confirm_pass" placeholder="Confirm your password">
+                        </div>
+                        <div class="form-group d-flex justify-content-center">
+                            <input style="border-radius: 0 0 8px 8px;" class="form-control glass" type="password" name="mobile" placeholder="Phone number">
+                        </div>
+                        <span class="text-error"></span>
+                        <br>
+                        <button type="button" style="background-color: var(--hightlight); border-color: var(--hightlight);" class="btn btn-primary btn-lg  mb-3">Register</button>
+                        <div class="register-login">
+                            <p>Already have an account? <a href="#" class="login-link">Log in</a></p>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </div> 
+    </div>
 </main>
--- src/views/layout/404.php
+++ src/views/layout/404.php
@@ -5,10 +5,37 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>404</title>
+    <link rel="stylesheet" href="<?php echo url('src/public/vendors/bootstrap/css/bootstrap.css') ?>">
+    <link rel="stylesheet" href="<?php echo url('src/public/vendors/font-awesome-6-pro-main/css/all.css') ?>">
+    <link rel="stylesheet" href="<?php echo url('src/public/css/style.css') ?>">
+    <style>
+        .bg_404 { 
+            background-image: url(<?php echo url('src/public/assets/imgs/404.jpg') ?>);
+        background-size: cover;
+
+            background-repeat: no-repeat;
+            height: 100vh;
+            display: flex;
+            align-items: center;
+        }
+    </style>
 </head>
 <body>
-    <h1>
-        PAGE NOT FOUND
-    </h1>
+    <div class="bg_404">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="404_img">
+                        <img src="<?php echo url('src/public/assets/imgs/404_img.png') ?>" alt="">
+                    </div>
+                    <h2>Page not found</h2>
+                    <p>Sorry, we can't find the page you're looking for</p>
+                    <div class="btn">
+                        <button>BACK TO HOME</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
