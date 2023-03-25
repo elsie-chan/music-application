88d50e0380bd4c9da2cb4360301a727291c604ee
Lam Nhu Trinh <lamnhutrinh@gmail.com>
2023-03-26 00:58:53 +0700

    add playlistView page

---
+++ src/views/playlistView.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Playlist</title>
+
+    <link rel="stylesheet" href="<?php echo url('src/public/vendors/bootstrap/css/bootstrap.css')?>">
+    <link rel="stylesheet" href="<?php echo url('src/public/vendors/font-awesome-6-pro-main/css/all.css')?>">
+    <link rel="stylesheet" href="<?php echo url('src/public/css/style.css') ?>">
+    <link rel="stylesheet" href="<?php echo url('src/public/css/playlistView.css')?>">
+    <style>
+        @import "<?php echo url('src/public/css/style.css') ?>";
+        body {
+            position: relative;
+        }
+
+        #playlist {
+            width: calc(100% - 200px);
+            left: 200px;
+            position: relative;
+            margin-bottom: 80px;
+        }
+
+        .sidebar {
+            z-index: 12;
+        }
+
+        #playlist {
+            z-index: 8;
+        }
+
+        .header {
+            z-index: 12;
+        }
+
+        .controlbar {
+            z-index: 20;
+        }
+
+    </style>
+</head>
+<body>
+    <?php require_once 'components/sidebar.php' ?>
+    <?php require_once 'components/controlbar.php' ?>
+    <div id="playlist">
+        <?php require_once 'components/header.php' ?>
+        <div class="row">
+            <div class="col bg-black">
+                <h1>day la cai playlist</h1>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
