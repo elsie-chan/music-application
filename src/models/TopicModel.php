5922ed1289c38a0a00d1c87cfd77140814170939
spessein <96380456+unicornizme2911@users.noreply.github.com>
2023-04-01 17:36:58 +0700

    update models and db

--- src/models/TopicModel.php
+++ src/models/TopicModel.php
@@ -2,5 +2,29 @@
 use App\Model\Model;
 class TopicModel extends Model{
     protected $table = 'topic';
-
+    public function __construct(){
+        parent::__construct();
+    }
+//  create
+    function add_topic($name_topic){
+        $sql = "SELECT * FROM `$this->table` WHERE `name_topics` = '$name_topic'";
+        $stmt = mysqli_query($this->con,$sql);
+        $response = array(
+            "error" => "",
+            "msg" => ""
+        );
+        if(mysqli_num_rows($stmt)==0){
+            $response["error"] = "Topic has not exists.";
+        }else{
+            $data = array();
+            while($row = mysqli_fetch_object($stmt)){
+                array_push($data,$row);
+            }
+            $response["msg"] = $data;
+        }
+        return $response;
+    }
+//    read/get
+//    update/ edit
+//    delete
 }
