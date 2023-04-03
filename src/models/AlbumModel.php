115189bb5acf43b7e60f67d946dbfb7987eda26e
spessein <96380456+unicornizme2911@users.noreply.github.com>
2023-04-03 13:50:33 +0700

    update model and db

--- src/models/AlbumModel.php
+++ src/models/AlbumModel.php
@@ -1,1 +1,11 @@
 <?php
+use App\Model\Model;
+class AlbumModel extends Model
+{
+    protected $table = 'album';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
