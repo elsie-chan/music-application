717f3b93cca9a00932aaa3ef4d0df85c67e03d73
torondatto <nguyentrongdat1108@gmail.com>
2023-04-03 11:04:11 +0700

    update controller route

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -35,6 +35,7 @@ class AuthController extends Controller {
                     Redirect::to('/');
                 } else if ($response['msg']->role == 0) {
                     $_SESSION['user'] = $response['msg']->id_users;
+                    $_SESSION['username'] = $response['msg']->username;
                     Redirect::to('/');
                 }
             } else {
@@ -53,7 +54,6 @@ class AuthController extends Controller {
 
     public function handle_register() {
         $error = "";
-        $message = "";
 
         $token = empty($_SESSION['token']) ? generate_token() : $_SESSION['token'];
         if ($_POST) {
@@ -72,9 +72,8 @@ class AuthController extends Controller {
                 $response = $model_response->register($_SESSION['username'], $_SESSION['email'], $_SESSION['password'], $_SESSION['confirm_pass'], $_SESSION['mobile'], $_SESSION['token']);
                 $error = $response['error'];
                 if (empty($error)) {
+                    $this->liked_song();
                     Redirect::to('auth/login');
-                    $message = "Account has been created. Please login now";
-                    $_SESSION['message'] = $message;
                 } else {
                     $_SESSION['error'] = $error;
                     Redirect::to('auth/register');
@@ -90,5 +89,31 @@ class AuthController extends Controller {
        session_destroy();
        $_SESSION = [];
        Redirect::to('/');
+    }
+
+    public function liked_song() {
+        if ($_SESSION['error'] == '') {
+
+            $username = $this->is_user_name();
+            $img =  url('src/public/assets/imgs/bg_login.png');
+
+            $model_response = $this->load_model('UserModel');
+            $model_response_playlist = $this->load_model('PlaylistModel');
+
+            if ($img == NULL) {
+                require_once (assets('views/layout/404.php'));
+            }
+
+            if ($username == NULL) {
+                require_once (assets('views/layout/404.php'));
+            }
+
+            if (!isset($model_response) or !isset($model_response_playlist)) {
+                require_once (assets('views/layout/404.php'));
+            }
+
+            $response = $model_response->get_user_by_username($username);
+            $response_playlist = $model_response_playlist->add_playlist("liked_song", $img, date('Y-m-d H:i:s', time()), $response->id_users);
+        }
     }
 }
