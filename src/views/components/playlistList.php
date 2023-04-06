6345055d05ad6251a4d11f7715e83423aa8711a7
XID <86798664+Alexid-ID@users.noreply.github.com>
2023-04-06 13:48:52 +0700

    add component playlistList

---
+++ src/views/components/playlistList.php
@@ -0,0 +1,55 @@
+<style>
+    @import "<?php echo url('src/public/css/components/playlistList.css') ?>";
+</style>
+
+
+    <div class="row albums__list">
+        <div class="col-6 col-xl-2 col-md-3 col-sm-4">
+            <div class="album__item">
+                <a href="#">
+                    <img src="https://source.unsplash.com/random/1920x1080/?wallpaper,landscape" alt="">
+                    <p class="py-2">The song name 1</p>
+                </a>
+            </div>
+        </div>
+        <div class="col-6 col-xl-2 col-md-3 col-sm-4">
+            <div class="album__item">
+                <a href="#">
+                    <img src="https://source.unsplash.com/random/1920x1080/?wallpaper,landscape" alt="">
+                    <p class="py-2">The song name 1</p>
+                </a>
+            </div>
+        </div>
+        <div class="col-6 col-xl-2 col-md-3 col-sm-4">
+            <div class="album__item">
+                <a href="#">
+                    <img src="https://source.unsplash.com/random/1920x1080/?wallpaper,landscape" alt="">
+                    <p class="py-2">lana del rey did you know that there's a tunnel under ocean blvd</p>
+                </a>
+            </div>
+        </div>
+        <div class="col-6 col-xl-2 col-md-3 col-sm-4">
+            <div class="album__item">
+                <a href="#">
+                    <img src="https://source.unsplash.com/random/1920x1080/?wallpaper,landscape" alt="">
+                    <p class="py-2">The song name 1</p>
+                </a>
+            </div>
+        </div>
+        <div class="col-6 col-xl-2 col-md-3 col-sm-4">
+            <div class="album__item">
+                <a href="#">
+                    <img src="https://source.unsplash.com/random/1920x1080/?wallpaper,landscape" alt="">
+                    <p class="py-2">The song name 1</p>
+                </a>
+            </div>
+        </div>
+        <div class="col-6 col-xl-2 col-md-3 col-sm-4">
+            <div class="album__item">
+                <a href="#">
+                    <img src="https://source.unsplash.com/random/1920x1080/?wallpaper,landscape" alt="">
+                    <p class="py-2">The song name 1</p>
+                </a>
+            </div>
+        </div>
+    </div>
