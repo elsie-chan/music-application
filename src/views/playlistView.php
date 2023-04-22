ccdbc4ed17be0b441f253f2dec18030647439c35
Lam Nhu Trinh <lamnhutrinh@gmail.com>
2023-04-22 22:24:01 +0700

    test playlist

--- src/views/playlistView.php
+++ src/views/playlistView.php
@@ -25,7 +25,6 @@
             left: 200px;
             position: relative;
             margin-bottom: 80px;
-            min-height: 100vh;
         }
 
         .sidebar {
@@ -70,11 +69,11 @@
             <div class="back-side col">
             </div>
             <div class="front-side col py-md-4 pr-md-3 h-auto" style=" padding:15px;">
-                <div class="info__img card flex-md-column flex-row" style="border: none;background-color: transparent; top: 0;">
-                    <img src="<?php echo url($data['playlist']->playlists_image) ?>" alt="error" style="">
+                <div class="card flex-md-column flex-row" style="border: none;background-color: transparent; top: 0;">
+                    <img src="<?php echo url($data['playlist']->playlists_image) ?>" alt="loi" style="">
                     <div class="info">
                         <div class="title d-flex">
-                            <h2 class="info__name mr-auto pl-0 p-2"
+                            <h2 class="mr-auto pl-0 p-2"
                                 style="padding-left: 0 !important;"><?php echo $data['playlist']->name_playlists ?></h2>
                             <!-- Button trigger modal -->
                             <button type="button" class="edit-name btn" data-toggle="modal" data-target="#myModal">
@@ -86,6 +85,7 @@
                         <!-- <p></p> -->
                         <div class="functional d-flex">
                             <i class="play-song fa-solid fa-circle-play p-2 " style="padding-left: 0 !important;"></i>
+                            <i class="repeat-song fa-sharp fa-solid fa-repeat p-2"></i>
                             <i class="more-info fa-duotone fa-circle-ellipsis-vertical  ml-auto p-2"></i>
                             <ul class="more-function" style="display: none;">
                                 <li><a href="#">Edit playlist name</a></li>
@@ -98,44 +98,40 @@
 
         </div>
         <div class="col">
-            <ul class="list-unstyled mt-3 songs">
+            <ul class="list-unstyled mt-3 songs" data-playlist_id="<?php echo $data['playlist']->id_playlists ?>">
             </ul>
-
         </div>
     </div>
 </div>
 
 
 <!-- Modal edit playlist name-->
-<form action="<?php echo url('edit_playlist_of_user')?>" method="post" class="edit-name-modal">
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
-          aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Edit details</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="row">
-                        <div class="col-sm-4 col-12 avt-playlist">
-                            <img id="srcPlaylist" src="<?php echo url($data['playlist']->playlists_image) ?>" alt="avatar-playlist">
-                            <input accept="image/*" type='file' id="inputPlaylist" class="form-control-file "/>
-                            <i class="hide fa-regular fa-pen"></i>
-                        </div>
-                        <div class="col-sm-8 col-12 d-flex flex-column content-playlist">
-                            <input type="text" class="form-control mb-2" name="name_playlist" placeholder="New playlist name" value="<?php echo $data['playlist']->name_playlists ?>">
-                            <textarea name="description" id="" class="form-control description" cols="30" rows="10"
-                                      placeholder="Add an optional description "></textarea>
-                            <button type="submit" class="btn btn-primary saveChanges">Save changes</button>
-
-                        </div>
+<form class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+      aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Edit details</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-sm-4 col-12 avt-playlist">
+                        <img src="<?php echo url($data['playlist']->playlists_image) ?>" alt="avatar-playlist">
+                        <input accept="image/*" type='file' id="inputAvatar" class="form-control-file "/>
+                        <i class="hide fa-regular fa-pen"></i>
+                    </div>
+                    <div class="col-sm-8 col-12 d-flex flex-column content-playlist">
+                        <input type="text" class="form-control mb-2" name="name" placeholder="New playlist name">
+                        <textarea name="description" id="" class="form-control description" cols="30" rows="10"
+                                  placeholder="Add an optional description "></textarea>
                     </div>
                 </div>
-                <div class="modal-footer">
-                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Save changes</button>
             </div>
         </div>
     </div>
@@ -154,16 +150,14 @@
 <script src="<?php echo url('src/public/vendors/bootstrap/js/bootstrap.js') ?>"></script>
 <script src="<?php echo url('src/public/vendors/jquery/jquery.js') ?>"></script>
 <script src="<?php echo url('src/public/vendors/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/css/toastr.css" rel="stylesheet"/>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
 
 <script type="module">
     import ajaxRequest from '<?php echo url('src/public/js/ajaxRequest.js')?>'
 
+    // js for context menu
     const left = document.documentElement.clientWidth;
     const header = $(".header");
 
-    // js for context menu
     function clear() {
         $(".context-menu").each((index, menu) => {
             $(menu).css({
@@ -220,7 +214,7 @@
                     }
                 );
 
-                })
+            })
             // close the menu
             $('.playlist-list').on("mouseleave", function (e) {
                 $(this).css({
@@ -245,16 +239,11 @@
                     {song_id: songId},
                     function (data) {
                         console.log(data);
-                        if (data.error) {
-                            toastr.warning(data.error);
-                        } else {
-                            console.log('success added to liked song');
-                            toastr.success(data.message);
-                        }
+                        console.log('success added to liked song');
+                        console.log(songId);
                     },
                     function (err) {
                         console.log("add song to liked song error");
-                        toastr.error("Can't add to liked song");
                     },
                     {
                         async: false,
@@ -265,35 +254,39 @@
 
         // remove song from playlist
         $('.remove-song-from-playlist').each(function (index, remove_item) {
-                $(remove_item).click(function(e) {
-                    console.log("click on remove item")
-                    e.preventDefault();
+            $(remove_item).click(function(e) {
+                console.log("click on remove item")
+                e.preventDefault();
 
-                    // console.log('song id' ,findSongIdAttr($(this)))
-                    let songId = findSongIdAttr($(this));
-                    let playlistId = <?php echo $data['playlist']->id_playlists ?>;
-                    ajaxRequest(
-                        '<?php echo url('delete_song_of_playlist')?>',
-                        'POST',
-                        {
-                            id_song: songId,
-                            id_playlist: playlistId,
-                        },
-                        function (data) {
-                            console.log(data)
-                            console.log('deleted song')
-                            window.location.reload();
-                            toastr.warning(data.message);
-                        },
-                        function (err) {
-                            console.log(err)
-                            console.log("get song error");
-                        },
-                        {
-                            async: false,
-                        }
-                    );
-                });
+                // console.log('song id' ,findSongIdAttr($(this)))
+                let songId = findSongIdAttr($(this));
+                let playlistId = <?php echo $data['playlist']->id_playlists ?>;
+                ajaxRequest(
+                    '<?php echo url('delete_song_of_playlist')?>',
+                    'POST',
+                    {
+                        id_song: songId,
+                        id_playlist: playlistId,
+                    },
+                    function (data) {
+                        console.log(data)
+                        console.log('deleted song')
+                        console.log(songId);
+                        console.log(playlistId);
+                        window.location.reload();
+
+                    },
+                    function (err) {
+                        console.log(err)
+                        console.log(songId);
+                        console.log(playlistId);
+                        console.log("get song error");
+                    },
+                    {
+                        async: false,
+                    }
+                );
+            });
 
         })
 
@@ -317,9 +310,10 @@
                 // console.log('song id' ,findSongIdAttr($(this)))
                 let songId = findSongIdAttr($(this));
                 let playlistId = $(this).data('playlist-id');
-
+                console.log('song id' ,songId)
+                console.log('playlist id',playlistId)
                 ajaxRequest(
-                   '<?php echo url('add_song_to_playlist')?>',
+                    '<?php echo url('add_song_to_playlist')?>',
                     'POST',
                     {
                         id_song: songId,
@@ -328,15 +322,11 @@
                     function (data) {
                         console.log(data)
                         console.log('added song')
-                        if (data.error) {
-                            toastr.warning(data.error);
-                        } else {
-                            toastr.success(data.message);
-                            toastr.options.timeOut = 2000;
-                        }
+                        console.log(songId);
+                        console.log(playlistId);
                     },
                     function (err) {
-                       console.log(err)
+                        console.log(err)
                         console.log("get song error");
                     }
                 );
@@ -401,12 +391,22 @@
     })
 
 
+    // Display modal for edit playlist name
+    const myModal = $('#myModal');
+    myModal.modal({
+        show: false
+    })
+    $('.edit-name').click(function () {
+        console.log('hello');
+        myModal.modal('show');
+    });
+    // asignContextMenu();
+    // ajax to render playlist
+
 
     import playSongByClick from '<?php echo url('src/public/js/playSongByClick.js')?>'
+    import handleMusic from '<?php echo url('src/public/js/controlbarListener.js') ?>'
     $(document).ready(function () {
-
-
-        // ajax to render playlist
         $.ajax({
             url: '<?php echo url('get_song_of_playlist') ?>',
             type: 'POST',
@@ -415,25 +415,20 @@
             },
             success: function (data) {
                 console.log(data);
-                if (data.error) {
-                    $('.list-unstyled').text("Your playlist is empty. Let find something for your playlist. ")
-                    $('.list-unstyled').append("<a href=''>Go to search</a>")
-                    $('.list-unstyled').find('a').attr('href', '<?php echo url('search') ?>');
-                }
-                else {
-                    const template = data[0].map((song, index) => {
-                        return `
-                                    <li class="media" data-song_id="${song.id_songs}">
+                localStorage.setItem('songs', JSON.stringify(data[0]));
+                const template = data[0].map((song, index) => {
+                    return `
+                                    <li class="song media" data-song_id="${song.id_songs}" data-song-index="${index}">
                                         <div class="col-10">
                                             <div class="row media-left">
                                                 <div class="songThumbnail">
                                                     <img class="song__img--src" src="${song.image_song}" alt="song avatar" >
                                                     <span class="">
                                                         <i class="fa-duotone fa-play icon-play-song"></i>
-                                                        <img class="wave--icon" src="<?php echo url('src/public/assets/imgs/yes.gif')?>" alt="sound wave" style="width:40px;height:40px; object-fit: contain;">
+                                                        <img class="wave--icon" src="<?php echo url('src/public/assets/imgs/icon-playing.gif')?>" alt="sound wave" style="width:36x;height:36px; object-fit: contain;">
                                                     </span>
                                                 </div>
-                                                <div class="card-info song">
+                                                <div class="card-info">
                                                     <h6>${song.name_songs}</h6>
                                                     <a class="song__info--artist" href="<?php echo url('artist') ?>/${song.id_artists}">${get_artist_by_id(song.id_artists)}</a>
                                                 </div>
@@ -445,14 +440,13 @@
                                         </div>
                                     </li>
                                 `;
-                    })
+                })
 
-                    $('.list-unstyled').html(template);
-                    $('.number-of-songs span').text(data[1])
-                    asignContextMenu();
-                    playSongByClick('.media', '<?php echo url()?>');
-                }
-
+                $('.list-unstyled').html(template);
+                $('.number-of-songs span').text(data[1])
+                asignContextMenu();
+                handleMusic();
+                playSongByClick('.media', '<?php echo url()?>');
             },
             error: function (error) {
                 console.log(error);
@@ -484,98 +478,22 @@
             return name_artist;
         }
 
-    // Display modal for edit playlist name
-    const inputPlaylist = $('#inputPlaylist');
-    const srcAvatar = $('#srcPlaylist');
-    const imgAvatar = $('.info__img img');
-    const bntSaveChanges = $('.saveChanges');
-    const infoName = $('.info__name');
-    const myModal = $('#myModal');
-    // console.log(inputPlaylist)
 
-    myModal.modal({
-        show: false
-    })
-    $('.edit-name').click(function () {
-        // console.log('hello');
-        myModal.modal('show');
-    });
 
-    // inputPlaylist.on('change', function () {
-    //     srcAvatar.src = URL.createObjectURL(this.files[0]);
-    //     // console.log(this.files[0])
-    // });
 
-    function close_modal() {
-        myModal.removeClass('show')
-        myModal.css('display', 'none');
-        $('.modal-backdrop').remove();
-    }
-
-   inputPlaylist.on('change', function () {
-        srcAvatar.attr("src",  URL.createObjectURL(this.files[0]));
-   });
-
-    bntSaveChanges.on("click", function (e) {
-        e.preventDefault();
-        // console.log(inputPlaylist[0])
-        let playlist_id = <?php echo $data['playlist']->id_playlists ?>;
-        const playlistInfo = new FormData($('.edit-name-modal')[0]);
-        const name_playlist = $('input[name="name_playlist"]').val()
-
-        playlistInfo.set('id_playlist', playlist_id);
-        playlistInfo.set('name_playlist', name_playlist);
-        // console.log(playlistInfo.get('id_playlist'));
-        // console.log(playlistInfo)
-        // for (const entry of playlistInfo.entries()) {
-        //
-        //     console.log(entry);
-        // }
-        let oldName = '<?php echo $data['playlist']->name_playlists ?>';
-        // console.log(oldName);
-
-        let newName = $('input[name="name_playlist"]').val();
-        // console.log(newName);
-        if(oldName === newName || newName === '') {
-            playlistInfo.set('name_playlist', oldName);
-        } else {
-            playlistInfo.set('name_playlist', newName);
-        }
-        // console.log(inputPlaylist[0].files[0]);
-        playlistInfo.set('img', inputPlaylist[0].files[0]);
-        // console.log(playlistInfo.get('img'))
-
-        if ($('input[name="name_playlist"]').val() !== '') {
-            $.ajax({
-                url: "<?php echo url('edit_playlist_of_user')?>",
-                type: 'POST',
-                enctype: 'multipart/form-data',
-                data: playlistInfo,
-                cache: false,
-                contentType: false,
-                processData: false,
-                success: function (data) {
-
-                    console.log('success');
-                    console.log(data);
-                    infoName.text(data.name);
-                    imgAvatar.attr("src" , data.img);
-                    toastr.success(data.message);
-                    $('.loading').hide(1000);
-                },
-                error: function (err) {
-                    console.log('error')
-                    console.log(err);
-                }
-            })
-            // console.log('new name')
-           close_modal();
-        //    $('.loading').show()
-        }
-    });
 
     })
+
+
+
+
+
 
 </script>
+
+
+<!-- <script src='<?php echo url('src/public/js/contextMenu.js') ?>' type="text/javascript"></script> -->
+
+
 </body>
 </html>
